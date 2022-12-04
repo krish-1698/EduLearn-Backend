4011877f61563df437f21836e382ab1a2baadc3a
Kishan <kparamanayagam@gmail.com>
2022-12-04 11:26:05 +0530

    First commit

---
+++ src/test/java/com/example/EduLearn/EduLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EduLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
